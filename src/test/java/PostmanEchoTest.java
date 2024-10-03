540ee5b64aca777fc06a4bed95767a3f17c1f4ce
Evgeniy Gornakov <gornakovevgenij@yandex.ru>
2024-10-03 10:53:17 +0800

    right

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -18,7 +18,7 @@ class PostmanEchoTest {
 // Проверки
                 .then().log().all()
                 .statusCode(200)
-                .body("data", org.hamcrest.Matchers.equalTo("Hello, friend"))
+                .body("data", org.hamcrest.Matchers.equalTo("Hello, world"))
         ;
     }
 }
